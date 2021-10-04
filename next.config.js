fcf8306672e20aedddf7278905230d152151eb9f
Jonathan Saleh <jonsaleh@microsoft.com>
2021-10-04 15:41:38 -0700

    setup repro for TsconfigPathsPlugin issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.plugins.unshift(new TsconfigPathsPlugin({
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+    }));
+    return config;
+  },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Input  from '@mui/material/Input';
+import { addOne } from '../utils';
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -13,6 +15,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Input value={addOne(1)}/>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
